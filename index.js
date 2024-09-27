bca4e8c8eeaeb87edec69399830ca88f8da8a74b
Lis1Van <gameinoriginal@gmail.com>
2024-09-27 12:19:59 +0300

    start and finish homework_02

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+const fs = require('fs');
+const path = require('path');
 
+const baseFolder = path.join(__dirname, 'baseFolder');
+
+function createFolderStructure() {
+    if (!fs.existsSync(baseFolder)) {
+        fs.mkdirSync(baseFolder);
+    }
+
+    for (let i = 1; i <= 5; i++) {
+        const folderPath = path.join(baseFolder, `folder${i}`);
+
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath);
+        }
+
+        for (let j = 1; j <= 5; j++) {
+            const filePath = path.join(folderPath, `file${j}.txt`);
+            fs.writeFileSync(filePath, `This is file ${j} in folder ${i}`);
+        }
+    }
+}
+
+function printFolderContents(dir) {
+    const items = fs.readdirSync(dir);
+
+    items.forEach(item => {
+        const itemPath = path.join(dir, item);
+        const stats = fs.statSync(itemPath);
+
+        if (stats.isDirectory()) {
+            console.log(`${itemPath} - folder`);
+            printFolderContents(itemPath);
+        } else if (stats.isFile()) {
+            console.log(`${itemPath} - file`);
+        }
+    });
+}
+
+createFolderStructure();
+
+printFolderContents(baseFolder);
