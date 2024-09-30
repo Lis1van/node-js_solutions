c7ef4f259dd2803d6bf479fe5fbe8c7c4c129627
Lis1Van <gameinoriginal@gmail.com>
2024-09-30 12:05:24 +0300

    finish homework_03

--- userStorage.js
+++ userStorage.js
@@ -1,24 +1,27 @@
-const fs = require('node:fs/promises')
-const path = require('node:path')
+const fs = require('node:fs/promises'); // Импортируем модуль fs для работы с файловой системой
+const path = require('node:path'); // Импортируем модуль path для работы с путями файлов
 
 module.exports = {
+    // Функция для чтения данных пользователей из файла
     read: async () => {
         try {
-            const filePath = path.join(__dirname, 'user.json');
-            const fileContent = await fs.readFile(filePath, 'utf-8');
-            return fileContent ? JSON.parse(fileContent) : [];
+            const filePath = path.join(__dirname, 'user.json'); // Формируем полный путь к файлу user.json
+            const fileContent = await fs.readFile(filePath, 'utf-8'); // Читаем содержимое файла в кодировке UTF-8
+            return fileContent ? JSON.parse(fileContent) : []; // Если файл не пустой, парсим его содержимое в объект, иначе возвращаем пустой массив
         } catch (e) {
-            console.error('Ошибка чтения файла:', e.message);
-            throw e;
+            console.error('Ошибка чтения файла:', e.message); // Выводим сообщение об ошибке в консоль
+            throw e; // Пробрасываем ошибку дальше для обработки в вызывающем коде
         }
     },
+
+    // Функция для записи данных пользователей в файл
     write: async (users) => {
         try {
-            const filePath = path.join(__dirname, 'user.json');
-            await fs.writeFile(filePath, JSON.stringify(users, null, 2));
+            const filePath = path.join(__dirname, 'user.json'); // Формируем полный путь к файлу user.json
+            await fs.writeFile(filePath, JSON.stringify(users, null, 2)); // Записываем массив пользователей в файл в формате JSON с отступами для читаемости
         } catch (e) {
-            console.error('Ошибка записи файла:', e.message);
-            throw e;
+            console.error('Ошибка записи файла:', e.message); // Выводим сообщение об ошибке в консоль
+            throw e; // Пробрасываем ошибку дальше для обработки в вызывающем коде
         }
     }
 }
